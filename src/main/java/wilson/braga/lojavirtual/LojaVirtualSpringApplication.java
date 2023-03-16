f1365cf136c079095e5465be09a0d0b56f52b649
Braga <Braga@DESKTOP-EQ3OOJK>
2023-03-16 19:08:48 -0300

    commit inicial loja virtual

---
+++ src/main/java/wilson/braga/lojavirtual/LojaVirtualSpringApplication.java
@@ -0,0 +1,13 @@
+package wilson.braga.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wilson/braga/lojavirtual/LojaVirtualSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package wilson.braga.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
