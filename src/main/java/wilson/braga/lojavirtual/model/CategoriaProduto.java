c9c9c46238bcfaced4d5cc8c7188da6d0ed4a533
Braga <Braga@DESKTOP-EQ3OOJK>
2023-03-16 22:00:46 -0300

    Criada classe de Categoria Produto

--- src/main/java/wilson/braga/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/wilson/braga/lojavirtual/model/CategoriaProduto.java
@@ -12,16 +12,15 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1 )
-public class MarcaProduto implements Serializable  {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
 	
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "seq_categoria_produto")
 	private Long id;
 	
 	@Column(nullable = false, length = 255)
@@ -56,8 +55,10 @@ public class MarcaProduto implements Serializable  {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
+	
+	
 
 }
