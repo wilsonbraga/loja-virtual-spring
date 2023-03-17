39273c41b7f7c1298b5409182bbcd336c3d838cf
Braga <Braga@DESKTOP-EQ3OOJK>
2023-03-16 22:27:25 -0300

    criada a classe de Acesso

---
+++ src/main/java/wilson/braga/lojavirtual/model/Acesso.java
@@ -0,0 +1,67 @@
+package wilson.braga.lojavirtual.model;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "sec_acesso", sequenceName = "sec_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sec_acesso")
+	private Long id;
+
+	private String descricao; /* Acesso: ROLE-ADMIN OU ROLE_SECRETARIO */
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		return Objects.equals(id, other.id);
+	}
+		
+	
+	
+}
